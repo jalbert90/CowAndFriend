e52a52a31d1a65129a150b7a86e159bd1463e970
jacob <jalbert90@gmail.com>
2022-12-04 00:33:36 -0500

    Very, very naive approach.

--- CowAndFriend.cpp
+++ CowAndFriend.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solve() {
-	int n, x;
+	int n, x, ans = 1e9 + 5, maxD = 0;
 	cin >> n >> x;
 
 	vector<int> a(n);
 
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
+		maxD = max(maxD, a[i]);
 	}
 
-	return 0;
+	sort(a.begin(), a.end(), [](int x, int y) {return x > y;});
+
+	for (int i = 0; i < n; i++) {
+		if (x == a[i]) {
+			ans = min(ans, 1);
+			break;
+		}
+	}
+
+	if (2 * a[0] > x) {
+		ans = min(ans, 2);
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (x % a[i] == 0) {
+			ans = min(ans, x / a[i]);
+			break;
+		}
+	}
+
+	return ans;
 }
 
 int main() {
