84016444e8293376ffe00cf979d797deec800f34
jacob <jalbert90@gmail.com>
2022-12-05 22:02:38 -0500

    solved...

--- CowAndFriend.cpp
+++ CowAndFriend.cpp
@@ -10,23 +10,34 @@ int solve() {
 
 	vector<int> a(n);
 
+	bool oneHop = false;
+
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
-		
-		if (a[i] == x) return 1;
+
+		if (a[i] == x) oneHop = true;
 
 		maxD = max(maxD, a[i]);
 	}
 
+	if (oneHop) return 1;
+
 	//sort(a.begin(), a.end(), [](int x, int y) {return x > y;});
 
 	int count = 0;
-	while (x - 2 * maxD > 0) {
-		x = x - maxD;
-		count++;
+	
+	if (x % maxD == 0) {
+		ans = x / maxD;
+		return ans;
+	}
+	else if (maxD < x) {
+		count = x / maxD;
+	}
+	else {
+		count = 1;
 	}
 
-	ans = count + 2;
+	ans = count + 1;
 
 	return ans;
 }
